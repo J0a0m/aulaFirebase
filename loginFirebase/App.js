4395304b2bcff255206efacc3c6d4c9eabf30640
J0a0m <joao.pagano01@gmail.com>
2026-09-08 14:10:34 +0000

    fim da aula 08/09

--- loginFirebase/App.js
+++ loginFirebase/App.js
@@ -1,26 +1,3 @@
-<<<<<<< HEAD
-import { NavigationContainer } from "@react-navigation/native"
-import { createNativeStackNavigator } from "@react-navigation/native-stack"
-
-import Login from './screens/Login'
-import Cadastro from './screens/Cadastro1'
-import Home from './screens/Home'
-
-const Stack = createNativeStackNavigator()
-
-export default function App(){
-
-  return(
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = "Login" component={Login}/>
-        <Stack.Screen name = "Cadastro" component={Cadastro}/>
-        <Stack.Screen name = "Home" component={Home}/>
-      </Stack.Navigator>
-    </NavigationContainer>
-  )
-}
-=======
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
  
@@ -41,5 +18,3 @@ export default function App(){
     </NavigationContainer>
   )
 }
- 
->>>>>>> 285302b (mudanças gerais aula 08/09)
--- loginFirebase/screens/Home.js
+++ loginFirebase/screens/Home.js
@@ -1,34 +1,11 @@
-<<<<<<< HEAD
-    import { View, Text, Button } from "react-native"
-
-    export default function Home(navigation){
-        return(
-            <View>
-
-                <Text>Seja bem-vindo(a)!</Text>
-
-                <Text>Usuário:</Text>
-
-                <Button
-                    title="Sair"
-                    onPress={()=>alert("Sair em construção")}
-                />
-
-                
-
-            </View>
-        )
-
-    }
-=======
 import {View, Text, Button} from "react-native"
-import {auth} from "../config/firebase"
-import {sair} from "../services/auth" 
-
+import {auth} from '../config/firebase'
+import {sair} from "../services/auth"
+ 
 export default function Home(){
     async function realizarLogout() {
         await sair()
-        navigation.navigation("Login")
+        navigation.navigate('Login')
     }
     return(
         <View>
@@ -41,4 +18,3 @@ export default function Home(){
         </View>
     )
 }
->>>>>>> 285302b (mudanças gerais aula 08/09)
